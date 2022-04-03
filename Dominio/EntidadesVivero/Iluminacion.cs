8ac7008378510ede2675a563854cb85b43b90fcb
Natalia Didac Cordoba <naty_didac@hotmail.com>
2022-04-03 14:12:02 -0300

    Creacion de clases

---
+++ Dominio/EntidadesVivero/Iluminacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.EntidadesVivero
+
+
+public class Iluminacion
+{
+	public int id { get; set; }
+
+	public string tipoIluminacion { get; set; }
+}
